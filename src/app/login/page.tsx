9d20db390c980c33102fcd752d11af08dd4b888e
PSskal <ricpascual29@gmail.com>
2023-09-09 22:06:59 -0500

    add users public table en supabase

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { AuthButtonServer } from "@/app/components/auth-button-server";
+
+export default function Login() {
+
+    return (
+        <>
+            <section className="grid place-content-center min-h-screen">
+                <h1 className="text-xl font-bold mb-4">Inicia secion cin devTer</h1>
+
+                <AuthButtonServer />
+            </section>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 
-import { AuthButtonServer } from "@/components/auth-button-server"
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import { AuthButtonServer } from "./components/auth-button-server"
 
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
-  const { data: posts } = await supabase.from('post').select()
+  const { data: { session } } = await supabase.auth.getSession()
+  if (session === null) {
+    redirect('/login')
+  }
+
+  const { data: posts } = await supabase
+    .from('post')
+    .select('*,users(*)')
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AuthButtonServer />
